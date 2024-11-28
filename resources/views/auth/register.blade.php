3f200aff4eb23beafc7f0f26a1cb31886979d2d5
amru404 <amru.azzam3@gmail.com>
2024-11-28 13:28:18 +0700

    update register role

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,6 +10,8 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
+                        
+                        <input type="hidden" value="super-admin" name="role">
 
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
